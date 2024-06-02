ce24fe62e124e14f272bda8d109cbcbeaebc65d8
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-06-02 11:12:31 -0600

    feat: Add routing and styles for main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,98 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Welcome from './components/WelcomePage';
+import { AnimatePresence, motion } from 'framer-motion';
+import About from './components/AboutPage';
+import ProjectsPage from './components/ProjectsPage';
 
-function App() {
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0.9
+  },
+  animate: {
+    opacity: 1,
+    scale: 1
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.9
+  }
+};
+
+const pageTransition = {
+  duration: 0.5,
+  type: 'spring',
+  bounce: 0.25
+};
+
+function AnimatedRoutes() {
+  const location = useLocation();
 
   return (
-    <>
-    </>
-  )
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={
+          <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageVariants}
+            transition={pageTransition}
+            style={{ position: 'absolute', width: '100%' }}
+          >
+            <Welcome />
+          </motion.div>
+        } />
+        <Route path="/about" element={
+          <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageVariants}
+            transition={pageTransition}
+            style={{ position: 'absolute', width: '100%' }}
+          >
+            <About />
+          </motion.div>
+        } />
+        <Route path="/projects" element={
+          <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageVariants}
+            transition={pageTransition}
+            style={{ position: 'absolute', width: '100%' }}
+          >
+            <ProjectsPage />
+          </motion.div>
+        } />
+        <Route path="/contact" element={
+          <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageVariants}
+            transition={pageTransition}
+            style={{ position: 'absolute', width: '100%' }}
+          >
+            <h1>Contact</h1>
+          </motion.div>
+        } />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <Header />
+      <AnimatedRoutes />
+    </Router>
+  );
+}
+
+export default App;
